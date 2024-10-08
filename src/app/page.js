5a7aa9b9e701d0600c0148c16142232e9e1bbecc
promahbubul <mahbublalam500@gmail.com>
2024-10-08 19:23:27 +0600

    update typography [Home page]

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-black">
       <Header />
       <Catelog />
       <Footer />
