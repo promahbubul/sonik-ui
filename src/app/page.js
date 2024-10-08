35ed6383006269e541a6cff8e355e3c955168e07
promahbubul <mahbublalam500@gmail.com>
2024-10-08 07:43:10 +0600

    update package json and page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Catelog from "@/components/Catalog";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-
 export default function Home() {
   return (
     <div className="">
